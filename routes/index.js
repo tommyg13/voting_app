e45d8e5e2751fc8a7466a2cbd646a474559b27cb
thomas <thomasgaz@hotmail.com>
2017-04-13 09:46:06 +0000

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get("/",(req,res)=>{
  If a user isn't logged in, they'll be redirected back to the login page.
  */
 function requireLogin (req, res, next) {
-  if (req.session.user===undefined) {
+  if (req.session.user===undefined && req.user===undefined) {
 
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
      req.session.destroy();
@@ -24,7 +24,7 @@ function requireLogin (req, res, next) {
     
     next();
   }
-};
+}
 
 /*Render the profile page. */
 router.get('/profile', requireLogin, function(req, res) {
